902dbdedbdc07d26d5165dc5c8756aa3e9a5afe8
Patrick Billingsley <prbillingsley89@gmail.com>
2023-08-18 00:48:27 -0500

    Adds documentation to FileHelper

--- lib/src/mixins/file_helper.dart
+++ lib/src/mixins/file_helper.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:logging/logging.dart';
 
+/// A mixin to help with file generation and formatting
 mixin FileHelper {
   final _log = Logger('FileHelper');
 
+  /// Generates a file at the given path with the given contents.
+  /// The file is subsequently formatted.
   void generateFile({required String path, required String contents}) {
     File(path)
       ..createSync(recursive: true)
@@ -13,6 +16,8 @@ mixin FileHelper {
     formatFile(path);
   }
 
+  /// Formats the file located at the given path.
+  /// Logs if attempt was successful or not.
   void formatFile(String filePath) async {
     var result = await Process.run('dart', ['format', '--fix', filePath]);
     if (result.exitCode == 0) {
